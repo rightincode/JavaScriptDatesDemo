452997b418f8d32f223e381bed9580b450c709b2
Richard Taylor <rtaylor@newoaksoftware.com>
2016-05-08 00:31:06 -0400

    First Pass on jsDatesAPI

---
+++ JavaScriptDatesDmeo/JsDatesAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace JsDatesAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Json formatter configuration
+            JsonMediaTypeFormatter json = config.Formatters.OfType<JsonMediaTypeFormatter>().Single();
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            json.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            json.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            json.UseDataContractJsonSerializer = false;
+        }
+    }
+}
---
+++ JavaScriptDatesDmeo/JsDatesAPI/Controllers/JsDatesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Http;
+
+namespace JsDatesAPI.Controllers
+{
+    [RoutePrefix("api/jsdates")]
+    public class JsDatesController : ApiController
+    {
+        [Route(""), HttpGet]
+        public DateTime GetDate()
+        {
+            return DateTime.Now;
+        }
+
+        [Route("utc"), HttpGet]
+        public DateTime GetUtcDate()
+        {
+            return DateTime.UtcNow;
+        }
+
+        [Route("save"), HttpPost]
+        public string SaveDate([FromUri] DateTime postedDate)
+        {
+            string postedDateStr = postedDate.ToString("G");
+
+            return postedDateStr;
+        }
+
+        [Route("saveandreturn"), HttpPost]
+        public DateTime SaveAndReturnDate([FromUri] DateTime postedDate)
+        {
+            return postedDate;
+        }
+    }
+}
